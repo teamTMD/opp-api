007a0999ea66d4c0287d6d4261967a29aa4cb6de
Trenton Creamer <trentoncreamer@yahoo.com>
2023-11-06 22:56:12 -0800

    Added two more working REST API calls for customers table

--- milestone2/api/main.py
+++ milestone2/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from models import models
 from db.database import engine
-from routers import auth, todos, admin
+from routers import auth, todos, admin, customers
 
 # application
 app = FastAPI()
@@ -12,5 +12,7 @@ models.Base.metadata.create_all(bind=engine)
 
 # Set API endpoints on router
 app.include_router(auth.router)
+app.include_router(customers.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+
---
+++ milestone2/api/routers/customers.py
@@ -0,0 +1,78 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Path, HTTPException
+from pydantic import BaseModel, Field
+from starlette import status
+from sqlalchemy.orm import Session
+
+from routers.auth import get_current_user
+from routers.helpers import check_user_authentication
+
+from models.models import Todos
+from models.models import Users
+from db.database import SessionLocal
+
+router = APIRouter(prefix='/customers', tags=['customers'])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# when an API uses this, it will enforce authorization
+
+# TC - DB dependency is enforeced when used in an API
+user_dependency = Annotated[dict, (Depends(get_current_user))]
+
+
+
+@router.get("/read-all-customers")
+async def read_all_customers(user: user_dependency, db: db_dependency):
+    check_user_authentication(user)
+    return db.query(Users).all()
+
+# Delete customer by id
+@router.delete("/customer/{customer_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_customer(user: user_dependency, db: db_dependency, customer_id: int = Path(gt=0)):
+    # Update below to check if the user is an admin
+    check_user_authentication(user)
+
+    customer_model = db.query(Users).filter(Users.id == customer_id).first()
+    if customer_model is None:
+        raise HTTPException(status_code=404, detail='Customer not found')
+    db.query(Users).filter(Users.id == customer_id).delete()
+
+    db.commit()
+
+# @router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+#     check_user_authentication(user)
+
+#     todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get('id')).first()
+#     if todo_model is None:
+#         raise HTTPException(status_code=404, detail='Todo not found')
+#     db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get('id')).delete()
+
+#     db.commit()
+
+
+
+# # Update user password by id 
+# @router.put("/update-user-password/{user_id}", status_code=status.HTTP_200_OK)
+# async def update_user_password(user: user_dependency, db: db_dependency, user_id: int = Path(gt=0)):
+
+#     # Update below to check that it is the same user 
+#     check_user_authentication(user)
+
+#     user_model = (
+#         db.query(Users).filter(Users.id == user_id).filter(Users.id == user.get('id')).first()
+#     )
+#     if user_model is not None:
+#         return user_model
+#     raise HTTPException(status_code=404, detail='User not found')
