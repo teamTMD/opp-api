abef34f5fb9e63d7b1e8e78bf393c2a99194141d
Trenton Creamer <trentoncreamer@yahoo.com>
2023-11-07 19:34:53 -0800

    Main updated with routes

--- milestone2/api/main.py
+++ milestone2/api/main.py
@@ -13,7 +13,7 @@ models.Base.metadata.create_all(bind=engine)
 # Set API endpoints on router
 app.include_router(auth.router)
 app.include_router(customers.router)
-app.include_router(todos.router)
+# app.include_router(todos.router)
 app.include_router(admin.router)
 app.include_router(payment_type.router)
 
